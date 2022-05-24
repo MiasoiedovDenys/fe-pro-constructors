58ba119a6a35e2f8e3c794170bef097fafe3f75a
Miasoiedov Denys <grocerytrade@gmail.com>
2022-05-24 23:00:31 +0300

    added files

--- src/Book.js
+++ src/Book.js
@@ -13,4 +13,38 @@ import { User } from './User.js';
  * @property {User[]} likedUsers
  * @property {User} publicationBy
  */
-export function Book(title, year, publicationBy, authors) {}
+export function Book(title, year, publicationBy, authors) {
+    this.title = title;
+    this.authors = authors;
+    this.year = year;
+    this.publicationBy = publicationBy;
+    this.likedUsers = [];
+    authors.forEach((author) => {
+        author.books.push(this);
+    });
+
+    publicationBy.myBooks.push(this);
+
+    Object.defineProperty(this, 'suggestedBooks', {
+        get() {
+            return this.authors.reduce((accum, { books }) => {
+                const allBooks = books.filter((book) => book !== this);
+                return [...new Set([...accum, ...allBooks])];
+            }, []).join(', ');
+        }
+    });
+
+    Object.defineProperty(this, 'suggestedPublicators', {
+        get() {
+            return this.authors.reduce((accum, { books }) => {
+                const users = books.filter((user) => user !== this.publicationBy).map(({ name }) => name);
+
+                return [...new Set([...accum, ...users])]
+            }, []).join(', ');
+        }
+    });
+}
+
+
+const user = new User('Bohdan', 1893);
+const book = new Book('1987', 2021, user, []);
--- src/User.js
+++ src/User.js
@@ -10,4 +10,61 @@ import { Book } from './Book.js';
  * @property {User[]} friends
  * @property {Book[]} likes
  */
-export function User(name, date) {}
+export function User(name, date) {
+    this.name = name;
+    this.date = date;
+    this.myBooks = [];
+    this.friends = [];
+    this.likes = [];
+    this.addToFriends = function (friend) {
+        if(this.friends.includes(friend)) {
+            this.friends = this.friends.filter((user) => user !== friend);
+            friend.friends = friend.friends.filter((user) => user !== this);
+        } else {
+            this.friends.push(friend);
+            friend.friends.push(this);
+        }
+    };
+
+    this.removeFriend = this.addToFriends;
+    
+    this.likeBook = function (book) {
+        if (this.likes.includes(book)) {
+            this.likes = this.likes.filter((currentBook) => currentBook !== book);
+            book.likedUsers = book.likedUsers.filter((user) => user !== this);
+        }else {
+            this.likes.push(book);
+            book.likedUsers.push(this);
+        }
+        
+    }
+
+    this.unlikeBook = this.likeBook;
+
+
+    Object.defineProperty(this, 'friendsNames', {
+        get() {
+            let friendsNames = this.friends.map(({ name }) => name).join(', ');
+            return friendsNames;
+        }
+    });
+
+    Object.defineProperty(this, 'likedBooks', {
+        get() {
+            let booksNames = this.likes.map(({ title }) => title).join(', ');
+            return  booksNames;
+        }
+    });
+
+    Object.defineProperty(this, 'publishedBooks', {
+        get() {
+            let booksNames = this.myBooks.map(({ title }) => title).join(', ');
+            return  booksNames;
+        }
+    });
+
+
+}
+
+
+   
